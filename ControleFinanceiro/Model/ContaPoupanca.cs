70e2d091a1d95196d31b5135678a511518071eeb
SeeJay3 <connor20082003@gmail.com>
2024-10-23 21:13:45 -0300

    Classe abstrada conta, Saldo protegido, subclasses ContaEspecial e ContaPoupanca

---
+++ ControleFinanceiro/Model/ContaPoupanca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Model
+{
+    public class ContaPoupanca : Conta
+    {
+        public float TaxaDeRendimentoMensal {  get; set; }
+        public override void Deposito(decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Saque(decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Transferencia(Conta destino, decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
